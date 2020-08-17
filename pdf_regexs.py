459a831b26d7fbcae63af3eb8cd1eefdaf2a4cad
Ivan Chen <ivan.if.chen@gmail.com>
2020-08-16 21:12:46 -0400

    Finished regex for 13, 15, 16 on gsheet

--- pdf_regexs.py
+++ pdf_regexs.py
@@ -3,7 +3,7 @@ regexs = [
         'target': 'PMP_Exam_Prep_Questions_Answers_-_Explanations_Bn6th',
         'regexs': [
             # 'PMP Lite Mock Exam' and 'Knowledge Area Quiz'
-            # (1044 questions) TODO: didn't confirm
+            # (1044 questions)
             [r'Test Questions\n([\S\s]*?)Answer Key and Explanations', r'\d\. ([\S\s]*?)A\.']
         ]
     },
@@ -19,7 +19,7 @@ regexs = [
     {
         'target': 'PMI-ACP_Project_Management_Institute_Agile_Certified_Practitioner_exam_study_guide_2018',
         'regexs': [
-            # 'Practice Questions' (239 questions) TODO: didn't confirm
+            # 'Practice Questions' (239 questions)
             [r'\nReview Questions\n([\S\s]*?)(?:Chapter   |Appendix   )', r'\d\. ([\S\s]*?)A\.'],
         ]
     },
@@ -45,8 +45,33 @@ regexs = [
         'target': 'CAPM_in_Depth_2019_Bn6th',
         'regexs': [
             # 'Review Questions' at the end of each chapter
-            # (~187 questions) TODO: didn't confirm
+            # (~187 questions)
             [r'Review Questions\n([\s\S]*?)(?:CHAPTER|APPENDIX)', r'\d\. ([\S\s]*?)A\.']
         ]
-    }
+    },
+    {
+        'target': 'PMP_Exam_Prep_Questions_Answers_-_Explanations_1000_PMP_Practice_Questions_with_Detailed_Solutions_Bn5th',
+        'regexs': [
+            # (1024) Questions
+            [r'(?:Test |Practice\n)Questions\n([\s\S]*?)\n\nAnswers', r'\d\. ([\S\s]*?)A\. ']
+        ]
+    },
+    {
+        'target': 'PMP_Exam_Simplified_Bn5th',
+        'regexs': [
+            # Sample Questions (65 questions)
+            [r'\nQuick Review - Sample Questions([\s\S]*?)Quick Review - Solutions', r'\d\. ([\S\s]*?)a\. '],
+            # Sample Test (196 questions)
+            [r'FULL SAMPLE\s*?TEST([\s\S]*?)Solutions\n', r'A\d{3}\. ([\S\s]*?)a\. ']
+        ]
+    },
+    {
+        'target': 'PMP_Project_Management_Professional_Exam_Study_Guide_5th_2009_Bn4th',
+        'regexs': [
+            # Assessment Test (70 questions)
+            [r'\nAssessment Test\n(1\.[\s\S]*?)Answers to Assessment Test', r'\d\. ([\S\s]*?)A\. '],
+            # Review Questions (239 questions)
+            [r'\nReview Questions([\s\S]*?)\nAnswers to Review Questions', r'\d\. ([\S\s]*?)A\. ']
+        ]
+    },
 ]
